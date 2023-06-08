b13604ffe4fcf7f98636bdc8c61e2d0ff847850a
Federico <federuedaleal@hotmail.com>
2023-06-08 00:46:55 -0300

    Se agregan los componentes con su logica Listanoticias.jsx y Noticias.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,10 @@
-import { Form } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+import Listanoticias from "./Listanoticias";
 
 function Formulario() {
-  const [noticias, setNoticias] = useState([]);
   const [categoria, setCategoria] = useState("");
+  const [arrayNoticias, setArrayNoticias] = useState([]);
 
   useEffect(() => {
     consultarAPI();
@@ -11,44 +12,61 @@ function Formulario() {
 
   const consultarAPI = async () => {
     try {
-      const respuesta = await fetch('/api/noticias?category=' + categoria);
+      const respuesta = await fetch(
+        `https://newsdata.io/api/1/news?apikey=pub_239716e78b97b290380b8251513e029f94c32&category=${
+          categoria || "technology"
+        }&language=es`
+      );
       const noticias = await respuesta.json();
-      setNoticias(noticias);
+      setArrayNoticias(noticias.results);
     } catch (error) {
       alert("Error al obtener las noticias");
-    }
+        }
   };
-  
 
   return (
     <>
-      <Form>
-        <Form.Group>
-          <Form.Label>Buscar por categorías</Form.Label>
-          <Form.Select
-            name={categoria}
-            id="categoria"
-            onChange={(e) => {
-              setCategoria(e.target.value);
-            }}
-            value={categoria}
+      <Form className="p-3 border border-2 border-dark border-bottom-0">
+        <Form.Group
+          as={Row}
+          className="justify-content-center align-items-center"
+        >
+          <Form.Label
+            column
+            sm={2}
+            md={3}
+            htmlFor="categoria"
+            className="p-0 text-center text-lg-end mb-2 mb-md-0"
           >
-            <option value="">Elija una categoria</option>
-            <option value="business">Negocios</option>
-            <option value="entertainment">Entretenimiento</option>
-            <option value="environment">Medio Ambiente</option>
-            <option value="food">Comida</option>
-            <option value="health">Salud</option>
-            <option value="politics">Política</option>
-            <option value="science">Ciencia</option>
-            <option value="sports">Deportes</option>
-            <option value="technology">Tecnología</option>
-            <option value="top">Top noticias</option>
-            <option value="tourism">Turismo</option>
-            <option value="world">Mundo</option>
-          </Form.Select>
+            Buscar por categoría:
+          </Form.Label>
+          <Col sm={10} md={4}>
+            <Form.Select
+              name={categoria}
+              id="categoria"
+              onChange={(e) => {
+                setCategoria(e.target.value);
+              }}
+              value={categoria}
+            >
+              <option value="">Seleccionar categoria</option>
+              <option value="business">Negocios</option>
+              <option value="entertainment">Entretenimiento</option>
+              <option value="environment">Medio Ambiente</option>
+              <option value="food">Comida</option>
+              <option value="health">Salud</option>
+              <option value="politics">Política</option>
+              <option value="science">Ciencia</option>
+              <option value="sports">Deportes</option>
+              <option value="technology">Tecnología</option>
+              <option value="top">Top noticias</option>
+              <option value="tourism">Turismo</option>
+              <option value="world">Mundo</option>
+            </Form.Select>
+          </Col>
         </Form.Group>
       </Form>
+      <Listanoticias noticias={arrayNoticias} />
     </>
   );
 }
